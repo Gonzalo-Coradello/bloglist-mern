5a1d89e5053318da89784c719ae19a0a0280f953
Gonzalo Coradello <gonzalocoradello@gmail.com>
2024-02-17 16:58:48 -0300

    delete eslint rule

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -16,8 +16,6 @@ module.exports = {
   },
   plugins: ['react', 'jest', 'cypress'],
   rules: {
-    indent: ['error', 2],
-    'linebreak-style': ['error', 'windows'],
     quotes: ['error', 'single'],
     semi: ['error', 'never'],
     eqeqeq: 'error',
