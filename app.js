827569cba3e74953606f59d98a2b735a22d99a1d
Gonzalo Coradello <gonzalocoradello@gmail.com>
2024-02-18 12:33:37 -0300

    simulate failed health check

--- app.js
+++ app.js
@@ -31,6 +31,8 @@ app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw 'error...  '
   res.send('ok')
 })
 
